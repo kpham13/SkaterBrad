fa7f487e4633def175530028c13b06060222542f
Kevin Pham <kevin.v.pham@hotmail.com>
2014-10-28 15:25:55 -0700

    updated and reorganized brian/kori's code

--- SkaterBrad/GameScene.swift
+++ SkaterBrad/GameScene.swift
@@ -29,26 +29,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let heroCategory = 0x1 << 1
     let groundCategory = 0x1 << 2
   
-  override func didMoveToView(view: SKView) {
+    override func didMoveToView(view: SKView) {
     
-    //kori/brian
-    let trashCan = SKSpriteNode(imageNamed: "trashCan.gif")
-    let craneHook = SKSpriteNode(imageNamed: "crane.gif")
-    var bradJumpTexture = SKTexture(imageNamed: "")
-    var bradDuckTexture = SKTexture(imageNamed: "")
+        // Texture Variables
+        let trashCan = SKSpriteNode(imageNamed: "trashCan.gif")
+        let craneHook = SKSpriteNode(imageNamed: "crane.gif")
+        var bradJumpTexture = SKTexture(imageNamed: "")
+        var bradDuckTexture = SKTexture(imageNamed: "")
 
-    //spawns a trashcan every 2 seconds -kori/brian
-    let spawn  = SKAction.runBlock({() in self.spawnObstacles()})
-    let delay = SKAction.waitForDuration(NSTimeInterval(1.5))
-    let spawnThenDelay = SKAction.sequence([spawn, delay])
-    let spawnThenDelayForever = SKAction.repeatActionForever(spawnThenDelay)
-    self.runAction(spawnThenDelayForever)
-
-        // Swipe Recognizer Setup
-        var swipeRecognizer = UISwipeGestureRecognizer(target: self, action: "swipeAction:")
-        swipeRecognizer.direction = UISwipeGestureRecognizerDirection.Up
-        self.view?.addGestureRecognizer(swipeRecognizer)
-        
         // Swipe Recognizer Setup [Tuan/Vincent]
         var swipeUpRecognizer = UISwipeGestureRecognizer(target: self, action: "swipeUpAction:")
         swipeUpRecognizer.direction = UISwipeGestureRecognizerDirection.Up
@@ -86,13 +74,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         var bradTexture = SKTexture(imageNamed: "hero.jpg") // Change 90x90 image
         bradTexture.filteringMode = SKTextureFilteringMode.Nearest
         
-        //Tina/ brad jumping texture
+        // Hero Jumping Texture [Tina]
         bradJumpTexture.filteringMode = SKTextureFilteringMode.Nearest
         
-        //Tina/ brad ducking texture
+        // Hero Ducking Texture [Tina]
         bradDuckTexture.filteringMode = SKTextureFilteringMode.Nearest
         
-
         hero = SKSpriteNode(texture: bradTexture)
         hero.setScale(0.5)
         hero.position = CGPoint(x: self.frame.size.width * 0.35, y: self.frame.size.height * 0.5) // Change y to ground level
@@ -118,49 +105,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         self.addChild(ground)
         
+        // Spawns a Trash Can Every 2 Seconds [Brian/Kori]
+        let spawn  = SKAction.runBlock({() in self.spawnObstacles()})
+        let delay = SKAction.waitForDuration(NSTimeInterval(1.5))
+        let spawnThenDelay = SKAction.sequence([spawn, delay])
+        let spawnThenDelayForever = SKAction.repeatActionForever(spawnThenDelay)
+        self.runAction(spawnThenDelayForever)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-//        /* Called when a touch begins */
-//        
-//        for touch: AnyObject in touches {
-//            let location = touch.locationInNode(self)
-//            
-//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-//            
-//            sprite.xScale = 0.5
-//            sprite.yScale = 0.5
-//            sprite.position = location
-//            
-//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-//            
-//            sprite.runAction(SKAction.repeatActionForever(action))
-//            
-//            self.addChild(sprite)
-        }
-  
-  //kori and brian
-  func spawnObstacles(){
-    
-    let trashCan = SKSpriteNode(imageNamed: "trashCan.gif")
-    let craneHook = SKSpriteNode(imageNamed: "crane.gif")
-    
-    trashCan.position = CGPointMake(/*CGRectGetMinX(self.frame) +*/ self.frame.width, 75)
-    trashCan.size = CGSize(width: 35, height: 40)
-    trashCan.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 35, height: 40))
-    trashCan.physicsBody?.dynamic = false
-    trashCan.zPosition = 12
-    trashCan.name = "trashCan"
-    self.addChild(trashCan)
-    
-    craneHook.anchorPoint = CGPointMake(1.0, 5.0)
-    craneHook.position = CGPointMake((CGRectGetMaxX(self.frame) * 0.75),
-      CGRectGetMaxY(self.frame))
-    craneHook.size = CGSize(width: 60.0, height: 100.0)
-    self.addChild(craneHook)
-    
-  }
-  
+    }
   
     func swipeAction(swipe: UISwipeGestureRecognizer) {
         self.jumpMode = true
@@ -168,69 +122,68 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         println(self.jumpNumber)
         println(self.jumpTime)
         println(self.deltaTime)
+        
         // Jump Limit Logic ------ Uncomment to use.
 //        if self.jumpNumber < 2 && self.jumpTime <= 0.5 {
             self.hero.physicsBody!.velocity = CGVectorMake(0, 0)
-            self.hero.physicsBody!.applyImpulse(CGVectorMake(0, 35))
+            self.hero.physicsBody!.applyImpulse(CGVectorMake(0, 60))
             self.jumpNumber += 1
 //        }
     }
     
-
     override func update(currentTime: CFTimeInterval) {
         
-        //Kevin-Tina/ Moving background
+        // Moving Background [Kevin/Tina]
         self.enumerateChildNodesWithName("background", usingBlock: { (node, stop) -> Void in
             if let bg = node as? SKSpriteNode {
                 bg.position = CGPoint(x: bg.position.x-self.backgroundSpeed, y: bg.position.y)
+                
                 if bg.position.x <= -bg.size.width {
                     bg.position = CGPoint(x: bg.position.x+bg.size.width * 2, y: bg.position.y)
                 }
             }
-            
         })
         
-        //Kevin-Tina/ Moving road
+        // Moving Road [Kevin/Tina]
         self.enumerateChildNodesWithName("road", usingBlock: { (node, stop) -> Void in
             if let road = node as? SKSpriteNode {
                 road.position = CGPoint(x: road.position.x-self.roadSpeed, y: road.position.y)
+                
                 if road.position.x <= -road.size.width {
                     road.position = CGPoint(x: road.position.x+road.size.width * 2, y: road.position.y)
                 }
             }
-            
         })
       
-      //kori and brian
-      self.enumerateChildNodesWithName("trashCan", usingBlock: { (node, stop) -> Void in
-        if let trash = node as? SKSpriteNode {
-          trash.position = CGPoint(x: trash.position.x-self.roadSpeed, y: trash.position.y)
-          if trash.position.x < 0 {
-            trash.removeFromParent()
-            
-          }
+        //kori and brian
+        self.enumerateChildNodesWithName("trashCan", usingBlock: { (node, stop) -> Void in
+            if let trash = node as? SKSpriteNode {
+                trash.position = CGPoint(x: trash.position.x-self.roadSpeed, y: trash.position.y)
+                
+                if trash.position.x < 0 {
+                    trash.removeFromParent()
+                }
           
-        
-        if self.jumpMode == true {
-            self.currentTime = currentTime
-            self.deltaTime = self.currentTime - self.previousTime
-            self.previousTime = currentTime
-            self.jumpTime = self.jumpTime + self.deltaTime
-        }
+                if self.jumpMode == true {
+                    self.currentTime = currentTime
+                    self.deltaTime = self.currentTime - self.previousTime
+                    self.previousTime = currentTime
+                    self.jumpTime = self.jumpTime + self.deltaTime
+                }
+            }
+        })
     }
-})
-}
     
     // MARK: - HERO ACTIONS
     // [Tuan/Vincent]
     
     func swipeUpAction(swipe: UISwipeGestureRecognizer) {
-        
         self.jumpMode = true
         self.jumpTime = 0.0
         println(self.jumpNumber)
         println(self.jumpTime)
         println(self.deltaTime)
+        
         //         Jump Limit Logic ------ Uncomment to use.
         if self.jumpNumber < 2 && self.jumpTime <= 0.5 {
             self.hero.physicsBody!.velocity = CGVectorMake(0, 0)
@@ -247,6 +200,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func didBeginContact(contact: SKPhysicsContact) {
         println("Contact occured")
         self.jumpNumber = 0
+    }
+    
+    // MARK: - OBSTACLES
+    // [Brian/Kori]
+    
+    func spawnObstacles(){
+        
+        let trashCan = SKSpriteNode(imageNamed: "trashCan.gif")
+        let craneHook = SKSpriteNode(imageNamed: "crane.gif")
+        
+        trashCan.position = CGPointMake(/*CGRectGetMinX(self.frame) +*/ self.frame.width, 75)
+        trashCan.size = CGSize(width: 35, height: 40)
+        trashCan.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 35, height: 40))
+        trashCan.physicsBody?.dynamic = false
+        trashCan.zPosition = 12
+        trashCan.name = "trashCan"
+        self.addChild(trashCan)
+        
+        craneHook.anchorPoint = CGPointMake(1.0, 5.0)
+        craneHook.position = CGPointMake((CGRectGetMaxX(self.frame) * 0.75),
+            CGRectGetMaxY(self.frame))
+        craneHook.size = CGSize(width: 60.0, height: 100.0)
+        self.addChild(craneHook)
     }
     
 }
