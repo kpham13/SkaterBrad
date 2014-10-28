8b10b4bb6b11d3aaf5117cf348641d0d68d430f3
Tina Fu <TinasFu@outlook.com>
2014-10-28 13:15:21 -0700

    update moving background and road.

--- SkaterBrad/GameScene.swift
+++ SkaterBrad/GameScene.swift
@@ -11,11 +11,35 @@ import SpriteKit
 class GameScene: SKScene {
 
     var hero = SKSpriteNode()
+    var backgroundSpeed : CGFloat = 1.0
+    var roadSpeed : CGFloat = 5.0
+    var roadSize : CGSize?
 
     override func didMoveToView(view: SKView) {
 
         // Physics - setting gravity to game world
         self.physicsWorld.gravity = CGVectorMake(0.0, -9.8)
+        // Background
+        
+        for var index = 0; index < 2; ++index {
+            let bg = SKSpriteNode(imageNamed: "bg\(index).jpg")
+            bg.anchorPoint = CGPointZero
+            bg.position = CGPoint(x: index * Int(bg.size.width), y: 0)
+            bg.name = "background"
+            self.addChild(bg)
+        }
+        
+        // Roads
+        for var index = 0; index < 2; ++index {
+            let road = SKSpriteNode(imageNamed: "road.jpg")
+            road.anchorPoint = CGPointZero
+            road.position = CGPoint(x: index * Int(road.size.width), y: 0)
+            road.name = "road"
+            self.roadSize = road.size
+            self.addChild(road)
+        }
+
+        
         
         // Hero
         var bradTexture = SKTexture(imageNamed: "hero.jpg") // Change 90x90 image
@@ -34,26 +58,28 @@ class GameScene: SKScene {
         self.addChild(hero)
         
         // Ground
-        var groundTexture = SKTexture(imageNamed: "") // Add 336x112 image
+//        var groundTexture = SKTexture(imageNamed: "") // Add 336x112 image
+//        
+//        var sprite = SKSpriteNode(texture: groundTexture)
+//        sprite.setScale(2.0)
+//        sprite.position = CGPointMake(self.size.width / 2, sprite.size.height / 2)
+//
+//        self.addChild(sprite)
+//        
+//        var ground = SKNode()
+//        
+//        ground.position = CGPointMake(0, groundTexture.size().height)
+//        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, groundTexture.size().height * 2))
+//        ground.physicsBody?.dynamic = false
         
-        var sprite = SKSpriteNode(texture: groundTexture)
-        sprite.setScale(2.0)
-        sprite.position = CGPointMake(self.size.width / 2, sprite.size.height / 2)
 
-        self.addChild(sprite)
         
-        var ground = SKNode()
-        
-        ground.position = CGPointMake(0, groundTexture.size().height)
-        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, groundTexture.size().height * 2))
+        var ground = SKShapeNode(rectOfSize: CGSize(width: 400, height: self.roadSize!.height))
+        ground.hidden = true
+        ground.position = CGPoint(x: 0, y: self.roadSize!.height * 0.5)
+        ground.physicsBody = SKPhysicsBody(rectangleOfSize: self.roadSize!)
         ground.physicsBody?.dynamic = false
         
-//        var ground = SKShapeNode(rectOfSize: CGSize(width: 150, height: 100))
-//        ground.fillColor = UIColor.redColor()
-//        ground.position = CGPointMake(100,20)
-//        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: ground.frame.size.width, height: ground.frame.size.height))
-//        ground.physicsBody?.dynamic = false
-//        
         self.addChild(ground)
         
 //        /* Setup your scene here */
@@ -85,6 +111,29 @@ class GameScene: SKScene {
         }
     
     override func update(currentTime: CFTimeInterval) {
+        self.enumerateChildNodesWithName("background", usingBlock: { (node, stop) -> Void in
+            if let bg = node as? SKSpriteNode {
+                bg.position = CGPoint(x: bg.position.x-self.backgroundSpeed, y: bg.position.y)
+                if bg.position.x <= -bg.size.width {
+                    bg.position = CGPoint(x: bg.position.x+bg.size.width * 2, y: bg.position.y)
+                }
+            }
+            
+        })
+        
+        self.enumerateChildNodesWithName("road", usingBlock: { (node, stop) -> Void in
+            if let road = node as? SKSpriteNode {
+                road.position = CGPoint(x: road.position.x-self.roadSpeed, y: road.position.y)
+                if road.position.x <= -road.size.width {
+                    road.position = CGPoint(x: road.position.x+road.size.width * 2, y: road.position.y)
+                }
+            }
+            
+        })
+
+        
+        
+        
         //        /* Called before each frame is rendered */
     }
 
