f95103f917ca7fac7fecfd3b76896ab7fdc819ff
Sam Wong <sam.wong.uk@gmail.com>
2014-10-30 21:53:52 -0700

    add menu files

---
+++ SkaterBrad/SoundNode.swift
@@ -0,0 +1,34 @@
+//
+//  MusicNode.swift
+//  SkaterBrad
+//
+//  Created by Sam Wong on 30/10/2014.
+//  Copyright (c) 2014 Mother Functions. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import AVFoundation
+
+class SoundNode : SKNode {
+    
+    var audioPlayer : AVAudioPlayer! = nil
+    let backgoundMusicFile = "bgMusic"
+    
+    override init() {
+        super.init()
+        self.audioPlayer = AVAudioPlayer()
+        var musicURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(self.backgoundMusicFile, ofType: "mp3")!)
+        AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, error: nil)
+        AVAudioSession.sharedInstance().setActive(true, error: nil)
+        self.audioPlayer = AVAudioPlayer(contentsOfURL: musicURL, error: nil)
+        self.audioPlayer.prepareToPlay()
+        self.audioPlayer.play()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+}
