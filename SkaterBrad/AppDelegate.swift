f671be7229e057783675eada28d7a0dfa9717324
YOUR NAME <you@example.com>
2015-03-24 16:08:09 -0700

    Changed attribution to datalayer push. Waiting to see if correct

--- SkaterBrad/AppDelegate.swift
+++ SkaterBrad/AppDelegate.swift
@@ -41,61 +41,73 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TAGContainerOpenerNotifie
     
     func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
         if sourceApplication == "com.analyticspros.TestShop" as String! {
-//            if let urlQuery = url.query {
-//                let queryParameters = urlQuery.componentsSeparatedByString("&")
-//                var queryDictionary = [String: String]()
-//                for query in queryParameters {
-//                    let separatedQuery = query.componentsSeparatedByString("=")
-//                    let utm = separatedQuery[0]
-//                    let parameter = separatedQuery[1]
-//                    queryDictionary.updateValue(parameter, forKey: utm)
-//                }
-//                let utmCampaign = queryDictionary["utm_campaign"]
-//                let utmSource = queryDictionary["utm_source"]
-//                let utmMedium = queryDictionary["utm_medium"]
-//                let utmTerm = queryDictionary["utm_term"]
-//                let utmContent = queryDictionary["utm_content"]
-//                
-//                if utmCampaign == nil || utmSource == nil || utmMedium == nil {
-//                    println("Required Campaign Name, Source, and Medium")
-//                    return false
-//                }
-            if let urlQuery = url.query as String! {
-                //  Network Call to GA
-                let endpoint = "http://www.google-analytics.com/debug/collect?v=1&tid=UA-59762855-5&t=appview&cid"
-                let configuration = NSURLSessionConfiguration.ephemeralSessionConfiguration()
-                let session = NSURLSession(configuration: configuration)
-                let urlString = "\(endpoint)\(urlQuery)"
-                let URL = NSURL(string: urlString)
-                let request = NSMutableURLRequest(URL: URL!)
-                request.HTTPMethod = "GET"
-                let dataTask = session.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
-                    if let httpResponse = response as? NSHTTPURLResponse {
-                        let code = httpResponse.statusCode
-                        if code == 200 {
-                            println("Status Code: \(code)")
-                            var err : NSError?
-                            let parsedResponse: NSDictionary? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as? NSDictionary
-                            println("Parsed JSON Response: \(parsedResponse!)")
-                        }
-                        else {
-                            println("Status Code: \(code)")
-                        }
-                    }
-                    else {
-                        println("Error: \(error.description)")
-                    }
-                })
-                dataTask.resume()
+            if let urlQuery = url.query {
+                let queryParameters = urlQuery.componentsSeparatedByString("&")
+                var queryDictionary = [String: String]()
+                for query in queryParameters {
+                    let separatedQuery = query.componentsSeparatedByString("=")
+                    let utm = separatedQuery[0]
+                    let parameter = separatedQuery[1]
+                    queryDictionary.updateValue(parameter, forKey: utm)
+                }
+                let utmCampaign = queryDictionary["utm_campaign"]
+                let utmSource = queryDictionary["utm_source"]
+                let utmMedium = queryDictionary["utm_medium"]
+                let utmTerm = queryDictionary["utm_term"]
+                let utmContent = queryDictionary["utm_content"]
                 
-                return true
+                if utmCampaign == nil || utmSource == nil || utmMedium == nil {
+                    println("Required Campaign Name, Source, and Medium")
+                    return false
+                }
+                let dataLayer = TAGManager.instance().dataLayer
+                dataLayer.push(["event" : "campaign-app-attribution",
+                    "utm-campaign" : utmCampaign!,
+                    "utm-source" : utmSource!,
+                    "utm-medium" : utmMedium!,
+                    "utm-content" : utmContent!,
+                    "utm-term" : utmTerm!])
             }
-            println("Error: No Query Parameters")
-            return false
+            return true
         }
-        println("Error: Launching App is not authorized to run Skater Brad")
         return false
     }
+
+//            if let urlQuery = url.query as String! {
+//                //  Network Call to GA
+//                let endpoint = "http://www.google-analytics.com/debug/collect?v=1&tid=UA-59762855-5&t=appview&cid"
+//                let configuration = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+//                let session = NSURLSession(configuration: configuration)
+//                let urlString = "\(endpoint)\(urlQuery)"
+//                let URL = NSURL(string: urlString)
+//                let request = NSMutableURLRequest(URL: URL!)
+//                request.HTTPMethod = "GET"
+//                let dataTask = session.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+//                    if let httpResponse = response as? NSHTTPURLResponse {
+//                        let code = httpResponse.statusCode
+//                        if code == 200 {
+//                            println("Status Code: \(code)")
+//                            var err : NSError?
+//                            let parsedResponse: NSDictionary? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as? NSDictionary
+//                            println("Parsed JSON Response: \(parsedResponse!)")
+//                        }
+//                        else {
+//                            println("Status Code: \(code)")
+//                        }
+//                    }
+//                    else {
+//                        println("Error: \(error.description)")
+//                    }
+//                })
+//                dataTask.resume()
+//                
+//                return true
+//            }
+//            println("Error: No Query Parameters")
+//            return false
+//        }
+//        println("Error: Launching App is not authorized to run Skater Brad")
+//        return false
     
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
